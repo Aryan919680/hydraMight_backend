a0145efd73449ae5c140dd3c82aff1c0e512e371
Aryan Malpotra <aryanmalpotra@gmail.com>
2026-06-15 18:21:38 +0530

    s

--- src/server.js
+++ src/server.js
@@ -59,7 +59,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
-app.use(express.json({ limit: "5mb" }));
 app.use(express.json({ limit: '5mb' }));
 
 app.get('/health', (req, res) => {
